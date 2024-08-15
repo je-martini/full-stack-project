c5cff35fe36fed6b91b08821e989545809b9d7b9
je_martini <jesusmartini2017@gmail.com>
2024-08-15 09:05:38 -0400

    creating the routes with reaact-routes-dom to differnst views

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,24 +1,33 @@
-import "./index.css"
+import { useRoutes, BrowserRouter } from 'react-router-dom'
+
 import Home from '../Home'
 import MyAccount from '../MyAccount'
 import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+import "./index.css"
 
+const AppRoutes = () => {
 
+  let routes = useRoutes([
+    { path: '/',  element: <Home />},
+    { path: '/my-account',  element: <MyAccount />},
+    { path: '/my-order',  element: <MyOrder />},
+    { path: '/my-orders',  element: <MyOrders />},
+    { path: '/*',  element: <NotFound />},
+    { path: '/sign-in',  element: <SignIn />},
+  ])
 
-function App() {
+  return routes
+}
+
+const App = () => {
 
   return (
-      <>
-      <Home />
-      <MyAccount/>
-      <MyOrder/>
-      <MyOrders/>
-      <NotFound/>
-      <SignIn/>
-      </>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
   )
 }
 
